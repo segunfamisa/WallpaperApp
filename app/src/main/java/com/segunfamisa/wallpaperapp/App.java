976518434d4aafcd508a59cd0b9d1681c62c63a2
Segun Famisa <segunfamisa@gmail.com>
2016-02-13 14:16:58 +0100

    Init commit

---
+++ app/src/main/java/com/segunfamisa/wallpaperapp/App.java
@@ -0,0 +1,10 @@
+package com.segunfamisa.wallpaperapp;
+
+import android.app.Application;
+
+/**
+ * Created by Segun Famisa <segunfamisa@gmail.com> on 13/02/2016.
+ */
+public class App extends Application {
+
+}
